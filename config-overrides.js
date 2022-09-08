9d35a1825310cb6e8d04e7c43f1f541860be9ce4
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-09-08 17:02:54 +0900

    Feat: 절대경로 설정

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,19 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
-const { override, getBabelLoader } = require("customize-cra");
+const { override, getBabelLoader, addWebpackAlias } = require("customize-cra");
 
-module.exports = override(removeBuiltinBabelConfig, enableBabelConfig);
+module.exports = override(
+  removeBuiltinBabelConfig,
+  enableBabelConfig,
+  addWebpackAlias({
+    "@pages": path.resolve(__dirname, "src", "pages"),
+    "@components": path.resolve(__dirname, "src", "components"),
+    "@server": path.resolve(__dirname, "src", "server"),
+    "@modules": path.resolve(__dirname, "src", "modules"),
+    "@apis": path.resolve(__dirname, "src", "apis"),
+  })
+);
 
 function removeBuiltinBabelConfig(config) {
   const loader = getBabelLoader(config);
